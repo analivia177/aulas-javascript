68793fa613667a592fd5382ed8281999150579a1
ana linda <analinda@gmail.com>
2026-09-15 11:38:48 -0300

    adicionada pasta 15.09 - variaveis

---
+++ 15.09/script.js
@@ -0,0 +1,42 @@
+// let miranha = "Peter Parker";
+// let numero = 999999999;
+// let numero2= "99999999";
+// let idade = 17;
+//var nome = "cleiton";
+// nome = pedro 
+
+//const nome = "cleito";
+//nome = "pedro"; 
+
+
+// idade = 20;
+// idade = 39 
+
+// console.log("O homem-aranha é o " + nome + " e ele tem " + idade + " anos");
+
+// crie 4 variaveis contendo: nome, sobrenome, idade e curso 
+// Exiba no console em uma unica frase
+
+
+// let idade = "15";
+// var nome = "ana livia";
+// let curso = "javascript";
+// console.log(" eu sou a " + nome + " e tenho " + idade + " e faço o curso de " + curso);
+
+
+let nome = "ana livia"; //string
+let peso = 90; //int 
+let altura = 1.80; //float
+let funcionario = false; // bool - true == verdadeiro ou false == falso
+
+// let cidade = null; 
+// let estado; 
+
+// console.log(typeof nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionario);
+
+// console.log("A variavel nome tem " + nome + " e é do tipo " + typeof nome)
+ 
+console.log("a varaivel peso tem" + peso + "e é do tipo " typeof peso)
